f90db2e4a2d3e15f14d56f22506d8722f1d8edfa
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-01-06 01:00:13 -0600

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useMemo, useRef } from "react";
 
 interface User {
   id: number;
   username: string;
 }
 
+type fibFunc = (n: number) => number;
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+const myNum: number = 27;
+
 function App() {
   const [count, setCount] = useState<number>(0);
   const [users, setUsers] = useState<User[] | null>(null);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  console.log(inputRef?.current);
+  console.log(inputRef?.current?.value);
+
   useEffect(() => {
     console.log("mounting");
-  }, []);
-  return <div></div>;
+    console.log("Users:", users);
+    return () => console.log("unmounting");
+  }, [users]);
+
+  const addTwo = useCallback((): void => setCount((prev) => prev + 2), []);
+
+  const result = useMemo<number>(() => fib(myNum), []);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input ref={inputRef} type="text" />
+    </div>
+  );
 }
 
 export default App;
